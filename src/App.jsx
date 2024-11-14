0aef6eaf935f1b576f9f48972e71cf2ef15582f4
Rubén Guadalupe Zavala Castañón <rubengpe.zavala@gmail.com>
2024-11-13 21:36:27 -0600

    removed mounted state to enable or disable render of the component

--- src/App.jsx
+++ src/App.jsx
@@ -48,14 +48,9 @@ const FollowMouse = () => {
 }
 
 function App() {
-  const [mounted, setMounted] = useState(true)
-
   return (
     <main>
-      {mounted && <FollowMouse />}
-      <button onClick={() => setMounted(!mounted)}>
-        Toggle mounted FollowMouse
-        </button>
+      <FollowMouse />
     </main>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  //este no funciona en production solo en desarrollo se asegura que 
+  //este no funciona en production solo en desarrollo se asegura que
   //codigo corre correctamtene por ejemplo renderiza y hace cleanup de los useEfect
   ///en consola se puede ver como que se renderiza se hace cleanup y se vuelve a ejecutar
   //entoncs si queremos que eso desaparezca debemos quitar el tag StrictMode
-  <StrictMode> 
+  <StrictMode>
     <App />
   </StrictMode>,
 )
